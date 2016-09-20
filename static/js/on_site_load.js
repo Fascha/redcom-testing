9bbf0ba819bf172d444cbc99554096d070356122
Fascha <Fabian.Schatz@googlemail.com>
2016-09-20 11:47:12 +0200

    changed url for api

--- static/js/on_site_load.js
+++ static/js/on_site_load.js
@@ -8,8 +8,8 @@ $(document).ready(function(){
     var text_input_tree_thread_id = $("#text_input_tree_thread_id");
 
     button_tree_load.click(function(){
-        drawTree("http://127.0.0.1:5000/get_thread_by_id/" + text_input_tree_thread_id.val() + "/tree")
-        drawPie("http://127.0.0.1:5000/get_thread_by_id/" + text_input_tree_thread_id.val() + "/pie")
+        drawTree("/get_thread_by_id/" + text_input_tree_thread_id.val() + "/tree")
+        drawPie("/get_thread_by_id/" + text_input_tree_thread_id.val() + "/pie")
     });
 
 
