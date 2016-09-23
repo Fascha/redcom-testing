8f7d6576319deaeb2e9ea7b4c04f1b74c7b62bd6
Fascha <Fabian.Schatz@googlemail.com>
2016-09-23 11:16:43 +0200

    added ajax

--- static/js/on_site_load.js
+++ static/js/on_site_load.js
@@ -8,8 +8,35 @@ $(document).ready(function(){
     var text_input_tree_thread_id = $("#text_input_tree_thread_id");
 
     button_tree_load.click(function(){
-        drawTree("/get_thread_by_id/" + text_input_tree_thread_id.val() + "/tree")
-        drawPie("/get_thread_by_id/" + text_input_tree_thread_id.val() + "/pie")
+        // $.ajax({
+        //     url: "/get_thread_by_id/" + text_input_tree_thread_id.val() + "/tree",
+        //     type: "GET",
+        //     dataType: "json",
+        //     timeout: 0, //Set your timeout value in milliseconds or 0 for unlimited
+        //     success: function(response) { alert(response); },
+        //     error: function(jqXHR, textStatus, errorThrown) {
+        //         if(textStatus==="timeout") {
+        //             alert("Call has timed out"); //Handle the timeout
+        //         } else {
+        //             alert("Another error was returned"); //Handle other error type
+        //         }
+        //     }
+        // });​
+
+        $.ajax({
+            url: "/get_thread_by_id/" + text_input_tree_thread_id.val() + "/tree",
+            type: "GET",
+            dataType: "json",
+            timeout: 0,
+            success: drawTree,
+            error: function () {
+                alert("Error")
+            }
+
+        });
+
+        // drawTree("/get_thread_by_id/" + text_input_tree_thread_id.val() + "/tree");
+        drawPie("/get_thread_by_id/" + text_input_tree_thread_id.val() + "/pie");
     });
 
 
--- static/js/tree.js
+++ static/js/tree.js
@@ -4,7 +4,7 @@ function drawTree(data_for_tree){
 	var diameter = 480
 
 	var tree = d3.layout.tree()
-		.size([360, diameter / 2])
+		.size([360, diameter / 2 - 20])
 		.separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
 
 	var diagonal = d3.svg.diagonal.radial()
@@ -19,33 +19,56 @@ function drawTree(data_for_tree){
 		.attr("id","treesvg")
 		.attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
 
-	d3.json(data_for_tree, function(error, root) {
-		if (error) throw error;
+	// d3.json(data_for_tree, function(error, root) {
+	// 	if (error) throw error;
+    //
+	// 	var nodes = tree.nodes(root),
+	// 		links = tree.links(nodes);
+    //
+	// 	var link = svg.selectAll(".link")
+	// 		.data(links)
+	// 		.enter().append("path")
+	// 		.attr("class", "link")
+	// 		.attr("d", diagonal);
+    //
+	// 	var node = svg.selectAll(".node")
+	// 		.data(nodes)
+	// 		.enter().append("g")
+	// 		.attr("class", "node")
+	// 		.attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
+    //
+	// 	node.append("circle")
+	// 		.attr("r", 4.5);
+    //
+	// 	// node.append("text")
+	// 	// 	.attr("dy", ".31em")
+	// 	// 	.attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
+	// 	// 	.attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)"; })
+	// 	// 	.text(function(d) { return d.name; });
+	// });
 
-		var nodes = tree.nodes(root),
-			links = tree.links(nodes);
 
-		var link = svg.selectAll(".link")
-			.data(links)
-			.enter().append("path")
-			.attr("class", "link")
-			.attr("d", diagonal);
 
-		var node = svg.selectAll(".node")
-			.data(nodes)
-			.enter().append("g")
-			.attr("class", "node")
-			.attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
+	var nodes = tree.nodes(data_for_tree),
+		links = tree.links(nodes);
 
-		node.append("circle")
-			.attr("r", 4.5);
+	var link = svg.selectAll(".link")
+		.data(links)
+		.enter().append("path")
+		.attr("class", "link")
+		.attr("d", diagonal);
 
-		// node.append("text")
-		// 	.attr("dy", ".31em")
-		// 	.attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
-		// 	.attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)"; })
-		// 	.text(function(d) { return d.name; });
-	});
+	var node = svg.selectAll(".node")
+		.data(nodes)
+		.enter().append("g")
+		.attr("class", "node")
+		.attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
+
+	node.append("circle")
+		.attr("r", 4.5);
+
+
+
 
 	d3.select(self.frameElement).style("height", diameter - 150 + "px");
 	
